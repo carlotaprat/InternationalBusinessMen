b41eacd268f02823b1c31bbf3232408e3cfd43b4
carlotaprat <carlota.prat@est.fib.upc.edu>
2019-09-18 12:53:05 +0200

    Initial Commit

---
+++ BeMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeMobile
+//
+//  Created by Carlota Prat on 18/9/19.
+//  Copyright © 2019 Carlota Prat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
